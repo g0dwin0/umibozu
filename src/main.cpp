ffa70e0846e4016799aad4a8f8f0fd47b1cc8622
g0dwin0 <21052794@student.hhs.nl>
2023-12-31 03:30:55 +0100

    removed cli11

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,23 @@
-#include "CLI11.hpp"
+// #include "CLI11.hpp"
+#include "cpu.h"
 #include "frontend/window.h"
 #include "gb.h"
-#include "io.hpp"
+// #include "io.hpp"
 using namespace Umibozu;
-int handle_args(int& argc, char** argv, std::string& filename) {
-  CLI::App app{"", "umibozu"};
-  app.add_option("-f,--file", filename, "path to ROM")->required();
-  
-  CLI11_PARSE(app, argc, argv);
-  return 0;
-}
 
-int main(int argc, char** argv) {
+int main() {
   GB gb;
   Frontend fe(gb);
-
-  std::string filename;
-  handle_args(argc, argv, filename);
-  fmt::println("{}", filename);
-  
-//  gb.load_cart(read_file("roms/smbl.gb"));
-   gb.load_cart(read_file(filename));
-//
-  
-  // OPTIMIZE: abstract this away
 
   fe.gb = &gb;
   gb.ppu.set_renderer(fe.renderer);
   gb.ppu.set_frame_texture(fe.state.ppu_texture);
-  gb.ppu.set_sprite_overlay_texture(fe.state.sprite_overlay_texture);
-
-  gb.ppu.bg_viewport     = fe.state.bg_viewport;
-  gb.ppu.sprite_viewport = fe.state.sprite_viewport;
 
   while (fe.state.running) {
-    gb.cpu.run_instruction();
-    if (gb.ppu.frame_queued) {
+    if (gb.cpu.status == Umibozu::SM83::Status::ACTIVE) {
+      gb.cpu.run_instruction();
+    }
+    if (gb.ppu.frame_queued || gb.cpu.status == Umibozu::SM83::Status::PAUSED) {
       fe.handle_events();
       fe.render_frame();
       gb.ppu.frame_queued = false;
