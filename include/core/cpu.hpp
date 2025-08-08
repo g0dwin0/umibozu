f1b7d0653209b1f62c4a0c9b125e9476693e31af
Godwin <godwin@amegaku.re>
2025-08-09 01:33:48 +0200

    replaced reg16 with union

--- include/core/cpu.hpp
+++ include/core/cpu.hpp
@@ -4,17 +4,19 @@
 #include <string>
 #include <unordered_map>
 
-#include "bus.h"
-#include "common.h"
-#include "mapper.h"
-#include "ppu.h"
-#include "timer.h"
+#include "bus.hpp"
+#include "common.hpp"
+#include "mapper.hpp"
+#include "ppu.hpp"
+#include "timer.hpp"
 
-#define VBLANK_INTERRUPT 0x40
-#define STAT_INTERRUPT 0x48
-#define TIMER_INTERRUPT 0x50
-#define SERIAL_INTERRUPT 0x58
-#define JOYPAD_INTERRUPT 0x60
+static constexpr u8 VBLANK_INTERRUPT = 0x40;
+static constexpr u8 STAT_INTERRUPT   = 0x48;
+static constexpr u8 TIMER_INTERRUPT  = 0x50;
+static constexpr u8 SERIAL_INTERRUPT = 0x58;
+static constexpr u8 JOYPAD_INTERRUPT = 0x60;
+
+static constexpr std::array<u8, 5> INTERRUPT_TABLE = {VBLANK_INTERRUPT, STAT_INTERRUPT, TIMER_INTERRUPT, SERIAL_INTERRUPT, JOYPAD_INTERRUPT};
 
 namespace Umibozu {
 
@@ -25,6 +27,7 @@ namespace Umibozu {
 
     SM83();
     ~SM83();
+
     Bus *bus = nullptr;
 
     std::unordered_map<SM83::STATUS, std::string> cpu_mode = {
@@ -34,70 +37,43 @@ namespace Umibozu {
         {   SM83::STATUS::PAUSED, "PAUSED"},
     };
 
-
-
-    struct REG_16 {
-      u8 &high;
-      u8 &low;
-
-      REG_16(u8 &A, u8 &B) : high(A), low(B) {};
-
-      REG_16 &operator++() {
-        if (low == 0xFF) { high++; }
-        low++;
-        return *this;
-      };
-
-      u16 operator++(int) {
-        u16 v = get_value();
-        ++*this;
-        return v;
-      };
-
-      REG_16 &operator--() {
-        if (low == 0x00) { high--; }
-        low--;
-        return *this;
-      };
-      u16 operator--(int) {
-        u16 v = get_value();
-        --*this;
-        return v;
-      };
-
-      size_t operator+(int c) const { return get_value() + c; }
-      size_t operator+(REG_16 &reg) const {
-        return get_value() + reg.get_value();
-      }
-      REG_16 &operator+=(REG_16 &reg) {
-        u16 val = get_value() + reg.get_value();
-        high    = (val & 0xFF00) >> 8;
-        low     = val & 0xFF;
-        return *this;
-      }
-      operator u16() { return get_value(); }
-
-      u16 operator&(int val) const { return get_value() & val; };
-
-      REG_16 &operator=(u16 val) {
-        high = (val & 0xFF00) >> 8;
-        low  = val & 0xFF;
-        return *this;
-      };
-
-      [[nodiscard]] u16 get_value() const { return ((high << 8) + low); }
-    };
-
-    const std::array<u8, 5> INTERRUPT_TABLE = {
-        VBLANK_INTERRUPT, STAT_INTERRUPT, TIMER_INTERRUPT, SERIAL_INTERRUPT,
-        JOYPAD_INTERRUPT};
+    bool ei_queued = false;
 
     // Registers
-    u8 A, F, B, C, D, E, H, L;
-    REG_16 AF{A, F};
-    REG_16 BC{B, C};
-    REG_16 DE{D, E};
-    REG_16 HL{H, L};
+    union {
+      u16 AF;
+      struct {
+        u8 F;
+        u8 A;
+      };
+    };
+
+    union {
+      u16 BC;
+      struct {
+        u8 C;
+        u8 B;
+      };
+    };
+
+    union {
+      u16 DE;
+      struct {
+        u8 E;
+        u8 D;
+      };
+    };
+
+    union {
+      u16 HL;
+      struct {
+        u8 L;
+        u8 H;
+      };
+    };
+
+    
+
     u16 SP        = 0xFFFE;
     u16 PC        = 0x0100;
     STATUS status = STATUS::PAUSED;
