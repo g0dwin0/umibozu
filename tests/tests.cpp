3d2f98c5898f34b7649083f29fb143f73f5a826d
g0dwin0 <21052794@student.hhs.nl>
2025-09-01 01:49:29 +0200

    feat: added testing with catch2

---
+++ tests/tests.cpp
@@ -0,0 +1,13 @@
+#include "catch2/catch_test_macros.hpp"
+#include "core/gb.hpp"
+
+static GB core = {};
+
+TEST_CASE("Mooneye - MBC1 Bits Bank 1") {
+    
+}
+
+TEST_CASE("Mooneye - MBC1 Bits Bank 2") {
+    
+}
+
